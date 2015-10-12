80f9a5f368a978061a45072494821ef5c98a6d10
MaxMEllon <mozi_kke@excite.co.jp>
2015-10-12 15:53:39 +0900

    test

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,7 +1,7 @@
 sample_num = 100
 
 (0...sample_num).each do |n|
-  print n.to_s << ' : '
+  prit n.to_s << ' : '
   puts 'Fizz' if n % 3 == 0
   puts 'Buzz' if n % 5 == 0
   puts 'FizzBuzz' if n % 3 == 0 && n % 5 == 0
