ccfd28ff50169c664cddca3c8eec19dc435591f7
MaxMEllon <mozi_kke@excite.co.jp>
2015-10-12 11:54:53 +0900

    first commit

---
+++ fizz_buzz.rb
@@ -0,0 +1,8 @@
+sample_num = 100
+
+(0...sample_num).each do |n|
+  print n.to_s << ' : '
+  puts 'Fizz' if n % 3 == 0
+  puts 'Buzz' if n % 5 == 0
+  puts 'FizzBuzz' if n % 3 == 0 && n % 5 == 0
+end
